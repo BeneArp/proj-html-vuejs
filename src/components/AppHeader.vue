43f92d30234fb1a0f4fae068f41c866ab362c438
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-07-16 20:28:43 +0200

    feat: started with the navigation bar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -13,16 +13,64 @@ import Countdown from "./Countdown.vue";
     <header>
         <Countdown/>
         <!-- countdown inizio evento -->
-        <nav class="row "></nav>
+        <nav class="row ">
+            <div class="col-2">
+                <img src="../assets/images/dark-logo.png" alt="logo" class="logo">
+            </div>
+            <div class="col-6 menu">
+                <ul>
+                    <li>
+                        <a href="#"><h1>Home</h1></a>
+                        <i class="fa-solid fa-chevron-down"></i>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <a href="#"><h1>Home</h1></a>
+                        <i class="fa-solid fa-chevron-down"></i>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <a href="#"><h1>Home</h1></a>
+                        <i class="fa-solid fa-chevron-down"></i>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <a href="#"><h1>Home</h1></a>
+                        <i class="fa-solid fa-chevron-down"></i>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <a href="#"><h1>Home</h1></a>
+                        <i class="fa-solid fa-chevron-down"></i>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <a href="#"><h1>Home</h1></a>
+                        <i class="fa-solid fa-chevron-down"></i>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     </header>
 </template>
 
 <style lang="scss" scoped>
     @use '../styles/partials/variables' as *;
+    @use '../styles/general.scss' as *;
 
     nav {
             height: 100px;
             background-color: pink;
-        }
+            width: 100vw;
+            justify-content: center;
+            .menu {
+                display: flex;
+            }
+    }
 
 </style>
