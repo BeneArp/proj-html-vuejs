d1527bd34ee243d85a90279acaeb6c8c4a1d1d03
BeneArp <benedetta.arpino@gmail.com>
2024-07-17 10:06:28 +0200

    feat: add layout footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -15,6 +15,7 @@
         <div class="container-lg">
 
             <div class="row">
+                <!-- sezione address -->
                 <div class="col-5">
                     <ul>
                         <!-- titolo elenco -->
@@ -25,19 +26,47 @@
                         <!-- indirizzo mail -->
                         <li><a href="#">support@maxcoach.com</a></li>
                         <!-- icone social -->
-                        
-                        <li></li>
+                        <li>
+                            <i class="fa-brands fa-square-facebook"></i>
+                            <i class="fa-brands fa-square-twitter"></i>
+                            <i class="fa-brands fa-instagram"></i>
+                            <i class="fa-brands fa-linkedin"></i>
+                        </li>
                     </ul>
                 </div>
+                <!-- sezione explore -->
                 <div class="col-2">
+                    <ul>
+                        <!-- titolo elenco -->
+                        <li><strong>Explore</strong></li>
 
+                        <li>Start here</li>
+                        <li>Success story</li>
+                        <li>Blog</li>
+                        <li>Courses</li>
+                        <li>Contact us</li>
+                    </ul>
                 </div>
+                <!-- sezione informations -->
                 <div class="col-2">
+                    <ul>
+                        <!-- titolo elenco -->
+                        <li><strong>Information</strong></li>
 
+                        <li>Membership</li>
+                        <li>Purchase guide</li>
+                        <li>Privacy policy</li>
+                        <li>Terms of services</li>
+                    </ul>
                 </div>
                 <div class="col-3">
-
+                    <span><strong>Instagram</strong></span>
+                    <span class="user">@maxcoach</span>
                 </div>
+            </div>
+
+            <div class="ms-box text-center">
+                <span>&#169 2020 Maxcoach. All Rights Reserved</span>
             </div>
 
         </div>
@@ -47,6 +76,7 @@
 
 <style lang="scss" scoped>
     @use '../styles/partials/variables' as *;
+    @use "@fortawesome/fontawesome-free/css/all.min.css";
 
     footer{
         background-color: $white;
@@ -55,13 +85,38 @@
 
         .container-lg{
             // debug
-            background-color: orange;
-            height: 600px;
+            // background-color: orange;
+            // height: 600px;
 
-            [class*="col-"]{
-                // debug
-                border: 2px solid black;
-                height: 600px;
+            // [class*="col-"]{
+            //     // debug
+            //     border: 2px solid black;
+            //     // height: 600px;
+            // }
+
+            li{
+                margin-top: 0.8em;
+            }
+
+            i{
+                font-size: 30px;
+                margin: 1em 1.5em 1em 0;
+                color: #b4b4b4;
+            }
+
+            .ms-box{
+                margin-top: 3em;
+                color: #b4b4b4;
+                font-size: 14px;
+            }
+
+            span{
+                display: inline-block;
+                margin-top: 0.8em;
+            }
+
+            .user{
+                color: $orange;
             }
         }
     }
