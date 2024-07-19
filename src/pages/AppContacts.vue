91876ae22ecae4d58e86e066f837b35779b0850f
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-07-19 12:30:14 +0200

    feat: merge

--- src/pages/AppContacts.vue
+++ src/pages/AppContacts.vue
@@ -1,11 +1,8 @@
 <script>
     import BreadCrumbs from '../components/AppAboutComponents/BreadCrumbs.vue'
     import Info from '../components/AppContactsComponents/Info.vue'
-<<<<<<< HEAD
     import AppForm from '../components/AppContactsComponents/AppForm.vue'
-=======
     import MapContacts from '../components/AppContactsComponents/MapContacts.vue';
->>>>>>> 964a6bdfbeaafeaf7eb46fe08ed04c893059658b
 
     export default{
         name: "AppContacts",
